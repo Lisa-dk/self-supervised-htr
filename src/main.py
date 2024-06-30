e5ff46729c1da7d9e2c6396718d85adbea24da64
Lisa-dk <lisadkoopmans@gmail.com>
2024-06-30 12:40:06 +0200

    cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -41,7 +41,6 @@ if __name__ == "__main__":
     parser.add_argument("--test", action="store_true", default=False)
     parser.add_argument("--valid", action="store_true", default=False)
     parser.add_argument("--beam_search", action="store_true", default=False)
-    parser.add_argument("--test_supervised", action="store_true", default=False)
 
     parser.add_argument("--epochs", type=int, default=500)
     parser.add_argument('--start_epoch', type=int, default=0)
@@ -50,7 +49,7 @@ if __name__ == "__main__":
     parser.add_argument("--loss", type=str, default="ctc")
     parser.add_argument("--pretrained", action="store_true", default=False)
 
-    parser.add_argument("--vgg_layer", type=int, default=16)
+    parser.add_argument("--vgg_layer", type=int, default=9)
     parser.add_argument("--max_word_len", type=int, default=max_text_length)
     parser.add_argument("--lr", type=float, default=0.001)
 
@@ -69,13 +68,11 @@ if __name__ == "__main__":
 
             path_from = os.path.join("..", "raw", args.dataset)
             if args.dataset == "iam_gan":
-                path_from = os.path.join("..", "..", "htr_self_supervised", "htr_self_supervised", "GANwriting")
+                path_from = os.path.join("..", "GANwriting_adapted") # Change path to location of GANwriting
                 getattr(data.preproc, f"preproc_iam")(path_from, dataset_path)
             else:
                 getattr(data.preproc, f"preproc_{args.dataset}")(path_from, dataset_path)
             
-
-    # os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     input_size = (64, 216, 1)
@@ -96,18 +93,19 @@ if __name__ == "__main__":
     vocab_size = num_classes + num_tokens
     
     # get data paths and labels (path, label)
-    # TODO: change function name to sth general
-
     if args.subset:
+        # Can be used for cross-validation, training/evaluation per predefined fold.
         print("Fold", args.fold)
         data_train, data_valid, data_test, wid_train, wid_valid, wid_test = read_iam_subset(dataset_path, args.max_word_len, n_fold=args.fold, synth=args.synth)
         data_test = IAM_data(data_test, input_size=input_size, tokenizer=tokenizer, num_images=num_style_imgs, wids=wid_test)
         test_loader = torch.utils.data.DataLoader(data_test, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4)
     elif args.dataset == "iam_gan":
+        # To train the supervised HTR on IAM-GEN-SIA
         data_train, data_valid, data_test, wid_train, wid_valid, wid_test = read_iam(dataset_path, args.max_word_len, synth=args.synth)
         data_test = IAM_data(data_test, input_size=input_size, tokenizer=tokenizer, num_images=num_style_imgs, wids=wid_test)
         test_loader = torch.utils.data.DataLoader(data_test, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4)
     else:
+        # (Self-)supervised HTR on IAM-HTR
         data_train, data_valid, data_test, oov_data_train, oov_data_valid, oov_data_test, wid_train, wid_valid, wid_test, oov_wid_train, oov_wid_valid, oov_wid_test = read_iam(dataset_path, args.max_word_len, synth=args.synth)
         
         oov_data_train = IAM_data(oov_data_train, input_size=input_size, tokenizer=tokenizer, num_images=num_style_imgs, wids=oov_wid_train)
@@ -122,7 +120,7 @@ if __name__ == "__main__":
         oov_data_test = IAM_data(oov_data_test, input_size=input_size, tokenizer=tokenizer, num_images=num_style_imgs, wids=oov_wid_test)
         oov_test_loader = torch.utils.data.DataLoader(oov_data_test, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=4)
 
-
+    # Training on IAM-HTR-OOV or -IV
     if args.oov:
         train_loader = oov_train_loader
     else:
@@ -138,11 +136,11 @@ if __name__ == "__main__":
         if not args.pretrained:
             
             if args.self_supervised:
-                htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size)
+                htr_model = Puigcerver(input_size=input_size, d_model=tokenizer.vocab_size)
                 if args.subset:
                     folder_name = f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}char-fold{args.fold}" if "vgg" in args.loss else f"{args.dataset}/{args.loss}-{args.max_word_len}char-fold{args.fold}"
                 else:
-                    folder_name =f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}char-gen3000-adam-lr0003-lrplat-pretr" if "vgg" in args.loss else f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-adam-lr0003-lrplat-pretr"
+                    folder_name =f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}char-gen3000-adam-lr0003-lrplat" if "vgg" in args.loss else f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-adam-lr0003-lrplat"
                 
                 if args.synth:
                     folder_name = folder_name + "-synth"
@@ -155,40 +153,49 @@ if __name__ == "__main__":
                 
             else:
                 htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size)
-                folder_name = f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-maxpool-synth"
+                folder_name = f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-maxpool-lrplat"
                 model_name = f"./htr_models/{folder_name}/htr_model_supervised-{args.start_epoch - 1}.model"
                 print(model_name)
             
-            # if args.synth:
-            #     folder_name = folder_name + "-synth"
+            if args.synth:
+                folder_name = folder_name + "-synth"
             
-            # if args.oov:
-            #     folder_name = folder_name + "-oov"
+            if args.oov:
+                folder_name = folder_name + "-oov"
 
+            # Load existing model (for testing or continued training)
+            # Change line 140 to Puigcerver_supervised if done for pretrained settings
             if os.path.exists(model_name):
                 print("loading model: ", model_name)
-                htr_model.load_state_dict(torch.load(model_name)) #load
+                htr_model.load_state_dict(torch.load(model_name)) 
         else:
+            # Training in the pretrained setting
             htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size)
-            model_name = f"./htr_models/iam/ce/htr_model_supervised-40.model"
-            folder_name = f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}chars-adam-lr001" if "vgg" in args.loss else f"{args.loss}-{args.max_word_len}chars-adam-lr001"
+            model_name = f"./htr_models/iam_gan/ce-10char-maxpool/htr_model_supervised-73.model"
+            folder_name =f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}char-gen3000-adam-lr0001-lrplat-pretr" if "vgg" in args.loss else f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-adam-lr0001-lrplat-pretr"
+            
+            if args.synth:
+                folder_name = folder_name + "-synth"
+            
+            if args.oov:
+                folder_name = folder_name + "-oov"
+            
             if os.path.exists(model_name):
-                print("loading pretrained model model: ", model_name)
-                htr_model.load_state_dict(torch.load(model_name)) #load
-                # htr_model.replace_head(tokenizer.vocab_size)
-
+                print("Loading pretrained model: ", model_name)
+                print(folder_name)
+                htr_model.load_state_dict(torch.load(model_name))
         
+        # Optimizer and lr schedulers
         if args.self_supervised:
             optimizer = torch.optim.Adam(htr_model.parameters(), lr=0.001)
-            #optimizer = torch.optim.RMSprop(htr_model.parameters(), lr=0.001, momentum=0.9)
-            # scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, 1.0, 0.25, 50)
             scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, verbose=True)
         else:
             optimizer = torch.optim.Adam(htr_model.parameters(), lr=0.0001)
-            #optimizer = torch.optim.RMSprop(htr_model.parameters(), lr=0.0001, momentum=0.9)
             scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.2, patience=5, verbose=True)
 
         trainer = HTRtrainer(htr_model, optimizer=optimizer, lr_scheduler=scheduler, device=device, tokenizer=tokenizer, loss_name=args.loss, self_supervised=args.self_supervised, folder_name=folder_name, vgg_layer=args.vgg_layer)
+        
+        # Model training
         if args.dataset == "iam_gan" or args.subset:
             trainer.train_model(train_loader=train_loader, valid_loader=valid_loader, oov_valid_loader=None, epochs=(args.start_epoch, args.epochs))
         else:
@@ -196,16 +203,17 @@ if __name__ == "__main__":
     
     elif args.test or args.valid:
         if args.test:
-            data_loader = test_loader
+            data_loader = test_loader # for oov results: change to oov_test_loader
         elif args.valid:
-            data_loader = oov_valid_loader
+            data_loader = valid_loader # for oov results: change to oov_valid_loader
         beam_search = False
        
         folder_name = f"{args.dataset}/{args.loss}-{args.vgg_layer}-{args.max_word_len}char-gen3000-adam-lr0003-lrplat-pretr-synth-oov" if "vgg" in args.loss else f"{args.dataset}/{args.loss}-{args.max_word_len}char-gen3000-maxpool-synth"
         if args.self_supervised:
             htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size).cuda()
             model_name = f"./htr_models/{folder_name}/htr_model_self_supervised-{args.start_epoch}.model"
-            # from torchaudio, so uses a silent token
+            
+            # from torchaudio, so uses a silent token "|"
             beam_search_decoder = ctc_decoder(lexicon=None,
                 tokens=[char for char in tokenizer.chars + "|" + '#'],
                 nbest=1,
@@ -216,7 +224,6 @@ if __name__ == "__main__":
         
         else:
             folder_name = f"{args.dataset}/{args.loss}-{args.max_word_len}char-maxpool"
-            #model_name = f"./htr_models/{folder_name}/htr_model_supervised-{args.start_epoch}.model"
             htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size).cuda()
             model_name = f"./htr_models/{folder_name}/htr_model_supervised-{args.start_epoch}.model"
             # from torchaudio, so uses a silent token
@@ -238,10 +245,11 @@ if __name__ == "__main__":
                 )
         
         print(model_name)
+        # Load model
         if os.path.exists(model_name):
-                print("Loading model: ", model_name)
-                htr_model.load_state_dict(torch.load(model_name)) #load
-                htr_model.eval()
+            print("Loading model: ", model_name)
+            htr_model.load_state_dict(torch.load(model_name)) 
+            htr_model.eval()
         else:
             print("Model not found")
             exit()
@@ -266,76 +274,6 @@ if __name__ == "__main__":
                 beam_search = beam_search_decoder(y_pred_soft)
                 y_pred = [tokenizer.decode(label[0].tokens * (label[0].tokens < 57)) for label in beam_search]
 
-            for (pd, gt) in zip(y_pred, gt_labels):
-                pd_cer, gt_cer = list(pd), list(gt)
-                dist = editdistance.eval(pd_cer, gt_cer)
-                
-                cer += dist / (max(len(pd_cer), len(gt_cer)))
-
-                pd_wer, gt_wer = pd.split(), gt.split()
-                dist = editdistance.eval(pd_wer, gt_wer)
-                wer += dist / (max(len(pd_wer), len(gt_wer)))
-            total += len(gt_labels)
-
-        print(f"CER: {cer / total}, WER: {wer / total}")   
-    elif args.test_supervised:
-        charset_base = string.ascii_lowercase + string.ascii_uppercase
-    
-        tokenizer = Tokenizer(chars=charset_base, max_text_length=max_text_length, self_supervised=True)
-
-        htr_model = Puigcerver_supervised(input_size=input_size, d_model=tokenizer.vocab_size).cuda()
-        model_name = f"./network/htr_model_supervised-ce-40.model"
-        
-        if os.path.exists(model_name):
-                print("Loading model: ", model_name)
-                htr_model.load_state_dict(torch.load(model_name)) #load
-                htr_model.eval()
-        else:
-            print("Model not found")
-            exit()
-
-        beam_search_decoder = ctc_decoder(lexicon=None,
-                tokens=[char for char in tokenizer.chars + "|" + '#'],
-                nbest=1,
-                beam_size=50,
-                blank_token = "#",
-                sil_token = "|"
-            )
-
-        # gen_model = GenModel_FC(tokenizer.maxlen, tokenizer.vocab_size - 1, tokenizer.PAD)
-        # # self.gen_model.load_state_dict(torch.load('./network/gen_model/gen_model-15all.model')) #load
-        # gen_model.load_state_dict(torch.load('./network/gen_model/gen_model-25half.model'))
-        # gen_model.eval()
-        # gen_model.to(device)
-
-        cer = 0
-        wer = 0
-        total = 0
-        for batch in tqdm(train_loader):
-            imgs, gen_imgs, gt_labels, _ = batch
-            imgs = imgs.to(device)
-            gt_labels = gt_labels.to(device)
-
-            imgs = imgs.to(device)
-            gen_imgs = gen_imgs.to(device).squeeze(2)
-            gt_labels = gt_labels.to(device)
-
-            # gt_labels_1hot = gt_labels.long()
-            # gt_labels_1hot = torch.nn.functional.one_hot(gt_labels_1hot, 56).float()
-
-            # synth_imgs = gen_model(gen_imgs, gt_labels_1hot)
-
-            y_pred = htr_model(imgs)
-
-            y_pred_soft = torch.nn.functional.softmax(y_pred, dim=2).detach().cpu()
-            y_pred_max = torch.max(y_pred_soft, dim=2).indices
-            gt_labels = gt_labels.detach().cpu().numpy()
-            gt_labels = [tokenizer.decode(label) for label in gt_labels]
-            y_pred = [tokenizer.decode(label) for label in y_pred_max]
-
-            # beam_search = beam_search_decoder(y_pred_soft)
-            # y_pred_bs = [tokenizer.decode(label[0].tokens * (label[0].tokens < 57)) for label in beam_search]
-            
             for (pd, gt) in zip(y_pred, gt_labels):
                 pd_cer, gt_cer = list(pd), list(gt)
                 dist = editdistance.eval(pd_cer, gt_cer)
